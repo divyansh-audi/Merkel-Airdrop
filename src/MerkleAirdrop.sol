ed9d23f4c8f3f09b5d900507dc0387fde973cfe9
divyansh-audi <divyanshtech06@gmail.com>
2025-07-14 21:47:08 +0530

    Main contrats created

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+    // some list of addresses
+    // Allow someone in the list to claim tokens
+
+    error MerkelAirdrop__InvalidProof();
+    error MerkelAirdrop__AlreadyClaimed();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    event Claim(address indexed account, uint256 indexed amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate using the accout and the amount,the hash --> leaf node
+        if (s_hasClaimed[account]) {
+            revert MerkelAirdrop__AlreadyClaimed();
+        }
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkelAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+        // SafeERC20.safeTransfer(i_airdropToken, account, amount); this is the correct signature of the function altho.
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+}
---
+++ test/MerkleAirdropTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "@forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xa58a42519e6d2f83c936953136cde4042cb7abeafec3b085d0c2e5091d1522e3;
+    uint256 AMOUNT = 25 ether;
+    uint256 AMOUNT_TO_SEND = AMOUNT * 4;
+    bytes32 proofOne = 0xae41e39d8f898c53893153e478b0f6f97cb5819b0b78c617e539f93edd568f78;
+    bytes32 proofTwo = 0xea59ad30b75fa97ba56428aca6099b1b28a3a0db3bcaa9cc3caedf03bf2c31bf;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivateKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        token.mint(token.owner(), AMOUNT_TO_SEND);
+        token.transfer(address(airdrop), AMOUNT_TO_SEND);
+        (user, userPrivateKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        // console.log("user address is :", user);
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT, PROOF);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("ending balance:", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT);
+    }
+}
